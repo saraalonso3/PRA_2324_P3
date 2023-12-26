3742fd0f3b8a10ac90ec5a026d6f7a3daaba7498
saraalonso3 <salonsop302@gmail.com>
2023-12-26 17:00:05 +0100

    He creado los ficheros de la clase BSTree

---
+++ BSTree.h
@@ -0,0 +1,142 @@
+#ifndef BSTREE_H
+#define BSTREE_H
+
+#include <ostream>
+#include <stdexcept>
+#include "BSNode.h"
+
+template <typename T> 
+class BSTree {
+    private:
+	    int nelem;
+	    BSNode<T> *root;
+	    BSNode<T>* search(BSNode<T>* n, T e) const{
+		    if(n == nullptr){
+			    throw std::runtime_error("Error en la busqueda");
+		    }
+		    else if(n->elem < e){
+			    return search(n->right, e);
+		    }
+		    else if(n->elem > e){
+			    return search(n->left, e);
+		    }
+		    else{
+			    return n;
+		    }
+	    }
+
+	    BSNode<T>* insert(BSNode<T>* n, T e){
+		    if(n == nullptr){
+			    return new BSNode<T>(e);
+		    }
+		    else if(n->elem == e){
+			    throw std::runtime_error("El elemento ya existe");
+		    }
+		    else if(n->elem < e){
+			    n->right = insert(n->right, e);
+		    }
+		    else{
+			    n->left = insert(n->left, e);
+		    }
+		    return n;
+	    }
+
+	    //muestra los elementos de menor a mayor
+	   void print_inorder(std::ostream &out, BSNode<T>* n) const{
+		    if(n->left != nullptr){
+			    print_inorder(out, n->left);
+			    out<<n->elem<<std::endl;
+		    }
+		    if(n->right != nullptr){
+			    print_inorder(out, n->right);
+			    out<<n->elem<<std::endl;
+		    }
+	   }
+
+
+	    BSNode<T>* remove(BSNode<T>* n, T e){ //falta el delete!!!
+		    if(n == nullptr){
+			    throw std::runtime_error("Error en la búsqueda");
+		    }
+		    else if (n->elem < e){
+			    n->right = remove(n->right, e);
+		    }
+		    else if (n->elem > e){
+			    n->left = remove(n->left, e);
+		    }
+		    else{
+			    if(n->left != nullptr && n->right != nullptr){
+				    n->elem = max(n->left);
+				    n->left = remove_max(n->left);
+			    }
+			    else if(n->left != nullptr){
+				    n = n->left;
+			    }
+			    else if(n->right != nullptr){
+				    n = n->right;
+			    }
+		    }
+		    return n;
+	    }
+
+	    T max(BSNode<T>* n) const{
+		    if(n == nullptr){
+			    throw std::runtime_error("Error en la búsqueda");
+		    }
+		    else if(n->right != nullptr){
+			    return max(n->right);
+		    }
+		    else{ //encuentra el elemento y lo devuelve
+			    return n->elem;
+		    }
+	    }
+
+	    BSNode<T>* remove_max(BSNode<T>*n){
+		    if(n->right == nullptr)
+			   if(n != nullptr){
+				   return n->left;
+		    }
+		    else{
+			    n->right = remove_max(n->right);
+			    return n;
+		    }
+	    }
+	    //void delete_cascade(BSNode<T>* n);
+
+        
+    public:
+	    BSTree();
+	    int size() const{
+	    	return nelem;
+	    }
+
+	    T search(T e) const{
+	    	return search(root, e)->elem;
+	    }
+
+	    T operator[](T e) const;
+	    
+	    void insert(T e){
+	    	root = insert(root, e);
+	    }
+	    	
+	    friend std::ostream& operator<<(std::ostream &out, const BSTree<T> &bst){
+		    bst.print_inorder(out, bst.root);
+		    return out;
+	    }
+
+	    void remove(T e){
+		    root = remove(root,e);
+	    }
+	    /*~BSTree(){
+		    delete_cascade(root);
+	    }  */
+
+
+
+
+        
+    
+};
+
+#endif
--- Makefile
+++ Makefile
@@ -6,5 +6,9 @@ bin/testHashTable: Dict.h HashTable.h TableEntry.h testHashTable.cpp
 	mkdir -p bin
 	g++ -o bin/testHashTable testHashTable.cpp HashTable.h
 
+bin/testBSTree: BSTree.h BSNode.h testBSTree.cpp
+	mkdir -p bin
+	g++ -o bin/testBSTree testBSTree.cpp BSTree.h
+
 clean:
 	rm -rf *.o *.gch bin
